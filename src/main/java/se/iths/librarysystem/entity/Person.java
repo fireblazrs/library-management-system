41ea56ff7ad054e3fb395ef82004816fbf5be564
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-16 19:38:05 +0100

    Add Person & PersonTest

---
+++ src/main/java/se/iths/librarysystem/entity/Person.java
@@ -0,0 +1,116 @@
+package se.iths.librarysystem.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private String dob;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+    public Person() {
+    }
+
+    public Person(String firstname, String lastname, String dob,
+                  String phoneNumber, String email, String address) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dob = dob;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Person(String firstname, String lastname, String dob, String phoneNumber, String email) {
+        this(firstname, lastname, dob, phoneNumber, email, "");
+    }
+
+    public Person(String firstname, String lastname, String dob, String phoneNumber) {
+        this(firstname, lastname, dob, phoneNumber, "", "");
+    }
+
+    public Person(String firstname, String lastname, String dob) {
+        this(firstname, lastname, dob, "", "", "");
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return Objects.equals(firstname, person.firstname) && Objects.equals(lastname, person.lastname)
+               && Objects.equals(dob, person.dob);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstname, lastname, dob);
+    }
+}
---
+++ src/test/java/se/iths/librarysystem/entity/PersonTest.java
@@ -0,0 +1,79 @@
+package se.iths.librarysystem.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PersonTest {
+
+    @Test
+    void getIdShouldReturnSetId() {
+        Person person = new Person("Stacey", "Thomas", "1992-05-23", "0723456789", "stacey@themail.com");
+        person.setId(21L);
+
+        Long result = person.getId();
+
+        assertThat(result).isEqualTo(21L);
+    }
+
+    @Test
+    void getEmailShouldReturnSetEmail() {
+        Person person = new Person("Stacey", "Thomas", "1992-05-23", "0723456789");
+        person.setEmail("stacey@themail.com");
+
+        String result = person.getEmail();
+
+        assertThat(result).isEqualTo("stacey@themail.com");
+    }
+
+    @Test
+    void getPhoneNumberShouldReturnSetPhoneNumber() {
+        Person person = new Person("Stacey", "Thomas", "1992-05-23");
+        person.setPhoneNumber("0723456789");
+
+        String result = person.getPhoneNumber();
+
+        assertThat(result).isEqualTo("0723456789");
+    }
+
+    @Test
+    void getFirstNameShouldReturnSetFirstName() {
+        Person person = new Person();
+        person.setFirstname("Per");
+
+        String result = person.getFirstname();
+
+        assertThat(result).isEqualTo("Per");
+    }
+
+    @Test
+    void getLastNameShouldReturnLastName() {
+        Person person = new Person();
+        person.setLastname("Andersson");
+
+        String result = person.getLastname();
+
+        assertThat(result).isEqualTo("Andersson");
+    }
+
+    @Test
+    void getDobShouldReturnSetDob() {
+        Person person = new Person();
+        person.setDob("1978-12-21");
+
+        String result = person.getDob();
+
+        assertThat(result).isEqualTo("1978-12-21");
+    }
+
+    @Test
+    void getAddressShouldReturnSetAddress() {
+        Person person = new Person();
+        person.setAddress("15 Meadows Lane, Wessex, England");
+
+        String result = person.getAddress();
+
+        assertThat(result).isEqualTo("15 Meadows Lane, Wessex, England");
+    }
+
+}
