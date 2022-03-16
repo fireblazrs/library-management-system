b4af6b3b919285164bd211be3e07c0d36173a73a
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-16 18:52:56 +0100

    Refactor field access level

---
+++ src/main/java/se/iths/librarysystem/entity/Person.java
@@ -0,0 +1,2 @@
+package se.iths.librarysystem.entity;public class Person {
+}
--- src/main/java/se/iths/librarysystem/entity/RoleEntity.java
+++ src/main/java/se/iths/librarysystem/entity/RoleEntity.java
@@ -11,7 +11,7 @@ public class RoleEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
     private String role;
 
     public RoleEntity() {
