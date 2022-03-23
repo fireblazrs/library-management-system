42d0eeb3479ad27a6c1103ca49646de7cc269bbb
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-23 09:13:58 +0100

    Add RestService & RestController

---
+++ src/main/java/se/iths/librarysystem/controller/RoleController.java
@@ -0,0 +1,4 @@
+package se.iths.librarysystem.controller;
+
+public class RoleController {
+}
---
+++ src/main/java/se/iths/librarysystem/service/RoleService.java
@@ -0,0 +1,7 @@
+package se.iths.librarysystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+}
