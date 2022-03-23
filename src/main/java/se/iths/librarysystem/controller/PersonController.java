9841f767bc43816e59e02af488e313b8bae70f05
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-23 12:54:11 +0100

    Add PersonController

---
+++ src/main/java/se/iths/librarysystem/controller/PersonController.java
@@ -0,0 +1,33 @@
+package se.iths.librarysystem.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.librarysystem.dto.Person;
+import se.iths.librarysystem.service.PersonService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("users")
+public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        Person newPerson = personService.createPerson(person);
+
+        return new ResponseEntity<>(newPerson, HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Person>> getAllUsers() {
+        List<Person> users = personService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
