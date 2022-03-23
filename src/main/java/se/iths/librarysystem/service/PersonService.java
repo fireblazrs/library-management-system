74156fb6df26c061cf4a40887b96ab00a24baf6c
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-23 12:54:01 +0100

    Add Person
    Add PersonService

--- src/main/java/se/iths/librarysystem/service/PersonService.java
+++ src/main/java/se/iths/librarysystem/service/PersonService.java
@@ -1,18 +1,53 @@
 package se.iths.librarysystem.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import se.iths.librarysystem.dto.Person;
+import se.iths.librarysystem.entity.PersonEntity;
+import se.iths.librarysystem.entity.RoleEntity;
 import se.iths.librarysystem.repository.PersonRepository;
+import se.iths.librarysystem.repository.RoleRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PersonService {
 
-    private final PersonRepository repository;
+    private final PersonRepository personRepository;
+    private final RoleRepository roleRepository;
+    private final ModelMapper modelMapper;
+    private final String DEFAULT_ROLE = "ROLE_USER";
 
-    public PersonService(PersonRepository repository) {
-        this.repository = repository;
+    public PersonService(PersonRepository repository, RoleRepository roleRepository, ModelMapper modelMapper) {
+        this.personRepository = repository;
+        this.roleRepository = roleRepository;
+        this.modelMapper = modelMapper;
     }
 
-    // CRUD
+    public Person createPerson(Person person) {
+        PersonEntity personEntity = modelMapper.map(person, PersonEntity.class);
+
+        RoleEntity defaultRole = roleRepository.findByRole(DEFAULT_ROLE);
+        personEntity.setRole(defaultRole);
+        PersonEntity savedPersonEntity = personRepository.save(personEntity);
+
+        return modelMapper.map(savedPersonEntity, Person.class);
+    }
+
+    public List<Person> getAllUsers() {
+        Iterable<PersonEntity> personEntities = personRepository.findAll();
+        List<Person> users = new ArrayList<>();
+        personEntities.forEach(user -> users.add(modelMapper.map(user, Person.class)));
+        return users;
+    }
+
+    public Person findById(Long id) {
+        PersonEntity personEntity = personRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("id " + id + " not found"));
+        //IdNotFoundException("user", id));
+        return modelMapper.map(personEntity, Person.class);
+    }
 
 
 }
