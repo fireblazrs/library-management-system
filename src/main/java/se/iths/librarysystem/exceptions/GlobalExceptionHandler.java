78e4807ba0e1f25aa69ea4ef87527a454878bdf2
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-23 14:36:14 +0100

    Add IdNotFoundException & GlobalExceptionHandler

---
+++ src/main/java/se/iths/librarysystem/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package se.iths.librarysystem.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+    @ExceptionHandler({IdNotFoundException.class})
+    public ResponseEntity<Object> idNotFoundException(IdNotFoundException exception) {
+        return buildResponseEntity(new ApiError(HttpStatus.NOT_FOUND, exception.getMessage(), exception.getPath()));
+    }
+}
