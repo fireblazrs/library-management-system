acfe8dc8ac9a859a2f316b43263879e48b62faf0
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-25 11:52:26 +0100

    Refactor PersonRepository

--- src/main/java/se/iths/librarysystem/repository/PersonRepository.java
+++ src/main/java/se/iths/librarysystem/repository/PersonRepository.java
@@ -4,14 +4,16 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import se.iths.librarysystem.entity.PersonEntity;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends CrudRepository<PersonEntity, Long> {
 
-    PersonEntity findByFirstnameAndLastname(String firstname, String lastname);
+    List<PersonEntity> findByFirstnameAndLastname(String firstname, String lastname);
 
-    PersonEntity findByFirstname(String firstname);
+    List<PersonEntity> findByFirstname(String firstname);
 
-    PersonEntity findByLastname(String lastname);
+    List<PersonEntity> findByLastname(String lastname);
 
     PersonEntity findBySsn(String ssn);
 
