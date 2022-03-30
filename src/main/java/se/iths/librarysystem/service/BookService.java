927bcf06a81ed9f1c85134ac14b8f2d007169381
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-30 15:02:21 +0200

    Add book endpoints

---
+++ src/main/java/se/iths/librarysystem/service/BookService.java
@@ -0,0 +1,27 @@
+package se.iths.librarysystem.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.librarysystem.entity.BookEntity;
+import se.iths.librarysystem.repository.BookRepository;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Iterable<BookEntity> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public BookEntity createBook(BookEntity bookEntity) {
+        return bookRepository.save(bookEntity);
+    }
+
+    public Iterable<BookEntity> getBooksByIsbn(String isbn) {
+        return bookRepository.findByIsbn(isbn);
+    }
+}
