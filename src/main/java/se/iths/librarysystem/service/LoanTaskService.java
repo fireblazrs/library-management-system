d0989a540931d2bb0b7ec49e630af4ee4c210be5
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-31 11:42:48 +0200

    Add LoanTaskService

---
+++ src/main/java/se/iths/librarysystem/service/LoanTaskService.java
@@ -0,0 +1,20 @@
+package se.iths.librarysystem.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.librarysystem.entity.LoanTaskEntity;
+import se.iths.librarysystem.repository.LoanTaskRepository;
+
+@Service
+public class LoanTaskService {
+
+    private final LoanTaskRepository taskRepository;
+
+    public LoanTaskService(LoanTaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public LoanTaskEntity createTask(LoanTaskEntity loanTask) {
+        return taskRepository.save(loanTask);
+    }
+
+}
