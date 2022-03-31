13a08833f0a218b1795a4839319d7ba94286ae45
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-31 08:51:09 +0200

    Refactor BookFormatEntity

--- src/main/java/se/iths/librarysystem/entity/BookFormatEntity.java
+++ src/main/java/se/iths/librarysystem/entity/BookFormatEntity.java
@@ -42,12 +42,12 @@ public class BookFormatEntity {
         this.length = length;
     }
 
-    public Long getId() {
-        return id;
-    }
-
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getFormatName() {
