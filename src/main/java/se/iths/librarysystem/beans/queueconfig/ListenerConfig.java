8a7d201bc986b14b7c35f4101739f25ae68bec3d
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-01 20:13:54 +0200

    Update ListenerConfig

--- src/main/java/se/iths/librarysystem/beans/queueconfig/ListenerConfig.java
+++ src/main/java/se/iths/librarysystem/beans/queueconfig/ListenerConfig.java
@@ -4,17 +4,22 @@ package se.iths.librarysystem.beans.queueconfig;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Configuration;
 import se.iths.librarysystem.entity.LoanTaskEntity;
+import se.iths.librarysystem.queue.ReceiverHandler;
 
 @Configuration
 public class ListenerConfig {
 
     private static final String QUEUE_NAME = "book-loan-queue";
+    private final ReceiverHandler receiverHandler;
+
+    public ListenerConfig(ReceiverHandler receiverHandler) {
+        this.receiverHandler = receiverHandler;
+    }
 
     @RabbitListener(queues = QUEUE_NAME)
     public void listen(LoanTaskEntity loanTask) {
-        System.out.println("Message from queue: LoanTask " + loanTask.getId() + " received!");
-
-        //do something with loanTask
+        receiverHandler.loanBook(loanTask);
+        System.out.println("Message from queue: LoanTask " + loanTask.getId() + " handled!");
     }
 
 }
