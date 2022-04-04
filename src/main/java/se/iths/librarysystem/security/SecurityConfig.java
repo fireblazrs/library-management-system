f0bc630c3e553e4b260973a9c808e05b91cb69f8
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-04 12:54:36 +0200

    Update SecurityConfig

--- src/main/java/se/iths/librarysystem/security/SecurityConfig.java
+++ src/main/java/se/iths/librarysystem/security/SecurityConfig.java
@@ -1,18 +1,26 @@
 package se.iths.librarysystem.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     // Man vill oftast ha CSRF skydd på "/home, /admin, /login
     // men behöver inte CSRF skyddet på apier
 
-    // first: create UserDetailsService & UserPrincipal - need to add password field & encryption
     private final String[] urls = {"/api/*", "/logout"};
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -22,15 +30,17 @@ public class SecurityConfig {
                 .httpBasic()
                 .and()
                 .authorizeRequests()
-                //.antMatchers("/roles").hasRole("ADMIN")
-                .antMatchers("/api/users/").permitAll() //be more specific about create user endpoint
+                .antMatchers("/api/roles/*").hasRole("ADMIN")
+                .antMatchers("/api/users/new", "/api/users/*", "/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .and()
                 .logout().permitAll()
-                .and().build();
+                .and()
+                .build();
+
     }
 
 }
