dddff56014b93550dc1689d838d647f6e5445ef5
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-08 15:32:34 +0200

    Add signup page

--- src/main/java/se/iths/librarysystem/view/MvcConfig.java
+++ src/main/java/se/iths/librarysystem/view/MvcConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/application").setViewName("application");
-        registry.addViewController("/admin").setViewName("admin");
-        registry.addViewController("/login").setViewName("login");
-    }
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/home").setViewName("home");
+//        registry.addViewController("/").setViewName("home");
+//        registry.addViewController("/application").setViewName("application");
+//        registry.addViewController("/admin").setViewName("admin");
+//        registry.addViewController("/login").setViewName("login");
+//    }
 
 }
