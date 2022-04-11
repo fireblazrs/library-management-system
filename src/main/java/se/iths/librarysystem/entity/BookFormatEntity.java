d64568e429d96cf0e20eac4c75f205026dfd57e1
Ahsanadam <adamebadul@gmail.com>
2022-04-11 16:55:41 +0200

    Modify Generation Type to IDENTITY

--- src/main/java/se/iths/librarysystem/entity/BookFormatEntity.java
+++ src/main/java/se/iths/librarysystem/entity/BookFormatEntity.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class BookFormatEntity {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     private String formatName;
--- src/main/java/se/iths/librarysystem/entity/RoomEntity.java
+++ src/main/java/se/iths/librarysystem/entity/RoomEntity.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 public class RoomEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "Room name is a required field")
